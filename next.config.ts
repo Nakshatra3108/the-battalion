2fed216c1274deed1f74c1bacf4b32a43f1d289a
Nakshatra Aggarwal <nakshmanyu@gmail.com>
2026-01-13 17:25:04 +0000

    Switch to battalion PartyKit project

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_PARTYKIT_HOST: process.env.NEXT_PUBLIC_PARTYKIT_HOST || 'shasn-game.nakshatra3108.partykit.dev',
+    NEXT_PUBLIC_PARTYKIT_HOST: process.env.NEXT_PUBLIC_PARTYKIT_HOST || 'battalion.nakshatra3108.partykit.dev',
   },
 };
 
